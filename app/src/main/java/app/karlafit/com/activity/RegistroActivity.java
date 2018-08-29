53debd351bfd00d01a84d5db707b1f6c463cddc1
dario alarcon <darcon111@hotmail.com>
2018-08-29 18:58:15 -0500

    cambios

---
+++ app/src/main/java/app/karlafit/com/activity/RegistroActivity.java
@@ -0,0 +1,15 @@
+package app.karlafit.com.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import app.karlafit.com.R;
+
+public class RegistroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
